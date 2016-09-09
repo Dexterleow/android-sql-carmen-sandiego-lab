ad6c22632dde83f75941b209393d6bce4487ee0b
dexterleow <gdleow@gmail.com>
2016-09-09 22:56:47 +0800

    Completed by need to verify the answer for question 8

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,38 +2,79 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT name, code from country where region = 'Southern Europe' ORDER BY population ASC limit 5;
 
+Ans: Holy See (Vatican City State)
+Country Code = 'VAT'
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT language from countrylanguage where countrycode character ='VAT';
 
+Ans: Language:  Italian.
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+SELECT countrycode, percentage from countrylanguage where language ='Italian' ORDER BY percentage DESC;
 
+SELECT name from country where code = 'SMR';
+
+Ans: San Marino.
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+SELECT name from city where countrycode = 'SMR';
 
+Ans:  Serravalle.
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT name from city where name like '%Serra%';
 
+Serra
+Tabo�o da Serra
+Itapecerica da Serra
+Serravalle
+
+SELECT countrycode from city where name = 'Serra';
+
+Ans: Bra. Brazil.
+
+SELECT name,code from country where region = 'South America';
+
+name       | code
+------------------+------
+Argentina        | ARG
+Bolivia          | BOL
+Brazil           | BRA
+Chile            | CHL
+Ecuador          | ECU
+Falkland Islands | FLK
+Guyana           | GUY
+Colombia         | COL
+Paraguay         | PRY
+Peru             | PER
+French Guiana    | GUF
+Suriname         | SUR
+Uruguay          | URY
+Venezuela        | VEN
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+SELECT capital from country where name ='Brazil';
 
+Ans. 211
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -52,6 +93,14 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT name,population from country where population > 90000 ORDER BY population ASC;
 
+name                  | population
+---------------------------------------+------------
+Virgin Islands, U.S.                  |      93000
+Grenada                               |      94000
+Tonga                                 |      99000
+Aruba                                 |     103000
+Saint Vincent and the Grenadines      |     114000
 
 -- She's in ____________________________!
